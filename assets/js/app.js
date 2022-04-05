9f42fb5ec6a166bdbf15c32a7e6748bd9c8bb47b
nh-polash <polashhoque0@gmail.com>
2022-04-05 13:00:35 +0600

    some features adding

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,14 @@
+// slick slider intialization
+
 $(document).ready(function(){
     $('.slider').slick({
         arrows: true,
+    });
+
+    $('.hot-deal-slider').slick({
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        dots: true,
     });
 });
